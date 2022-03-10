047b013b217baa86776d43c283edaf690ee438b1
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-03-11 01:07:43 +0700

    added font color

--- lib/constaints.dart
+++ lib/constaints.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const cPrimaryColor = Color(0xFFFCA2A2);
 const cPrimary2Color = Color(0xFFFFCCCC);
 const cSecondaryColor = Color(0xFFF9E1C7);
+const cFontColor = Color(0xFFFAAD5A);
