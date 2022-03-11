2c99ce2bb5cbda26449ed0a8b01dfb9486c5647f
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-03-11 21:53:46 +0700

    more screen completed

--- lib/screens/more/more.dart
+++ lib/screens/more/more.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'components/sections.dart';
+
 class MoreScreen extends StatefulWidget {
-  const MoreScreen({ Key? key }) : super(key: key);
+  const MoreScreen({Key? key}) : super(key: key);
 
   @override
   State<MoreScreen> createState() => _MoreScreenState();
@@ -10,8 +12,26 @@ class MoreScreen extends StatefulWidget {
 class _MoreScreenState extends State<MoreScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: const <Widget>[
+          SectionNameWithBorderColor(title: 'Settings',),
+          SubSection(title: 'Profile',),
+          SubSection(title: 'Address',),
+          SubSection(title: 'Bank Account/Credit card',),
+          SectionNameWithBorderColor(title: 'Helps',),
+          SubSection(title: 'Help Center',),
+          SubSection(title: 'Rules',),
+          SubSection(title: 'Privacy of KarKar',),
+          SubSection(title: 'About Us',),
+        ],
+      ),
     );
   }
 }
+
+
+
+
