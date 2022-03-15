3d7ab9b1370682e6be3b7aae4748393e39997de9
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-03-15 19:41:26 +0700

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'package:karkarapp/screens/home/home.dart';
 import 'package:karkarapp/screens/welcome/welcome.dart';
 import 'package:provider/provider.dart';
 import 'providers/cart_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
